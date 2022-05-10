456a2ea6e0853da2ffb663d423fb24f5675da32e
iamfranco <kinfaifrancochan@gmail.com>
2022-05-10 16:39:56 +0100

    Initial Commit

---
+++ AnimalProject/Pigeon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalProject
+{
+    internal class Pigeon : Bird
+    {
+        public Pigeon(string name, string color) : base(name, color)
+        {
+        }
+
+        public void EatPizza()
+        {
+            Console.WriteLine("Delicious pizza!");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("I'm a sleeping pigeon");
+        }
+    }
+}
---
+++ AnimalProject/Program.cs
@@ -0,0 +1,10 @@
+﻿using AnimalProject;
+
+Bird myBird = new Bird("Tweety", "Yellow");
+myBird.Speak();
+myBird.Fly();
+
+Pigeon pippa = new Pigeon("Pippa", "Grey");
+pippa.Speak();
+pippa.Fly();
+pippa.EatPizza();
